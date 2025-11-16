4cadbf356983f3dfebc30d2ca034051dfde89216
joako-code <pueblajoaquinunrc@gmail.com>
2025-11-16 02:47:20 -0300

    vuela opencvpruebas

--- OpencvPruebas/DemosB/Demo4.2/carcassone-detection/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Modelos de datos del sistema.
-"""
-
-from .tile import Tile
-from .board import Board
-
-__all__ = ['Tile', 'Board']
--- OpencvPruebas/DemosB/Demo4/carcassone-detection/steps/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Agregar el directorio actual al path
-sys.path.insert(0, os.path.dirname(__file__))
-
-from resize_step import ResizeStep
-
-__all__ = ['ResizeStep']
